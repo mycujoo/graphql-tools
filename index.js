e6eb075689ed65cae5c07a567734b1f08e34738a
Joeri Malmberg <joeri@mycujoo.tv>
2019-04-10 15:58:18 +0200

    Added a default http server to host your graphql endpoints from.

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ module.exports = {
   expressMiddlewares: require('./src/middlewares/express'),
   kafkaDataSourceWrapper: require('./src/datasources/kafkaDataSourceWrapper'),
   schemaFromAvro: require('./src/schemas/schemaFromAvro').schemaFromAvro,
+  getServer: require('./src/getServer'),
   // graphqlMiddlewares: require('./src/middlewares/graphql'),
 }
